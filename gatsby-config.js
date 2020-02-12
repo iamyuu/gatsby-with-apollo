514895a71f2a79421d9b1945bc487ef4b46819e2
iamyuu <akuyusuf27@gmail.com>
2020-02-12 10:58:02 +0700

    chore: upgrade & change some dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,5 @@
 module.exports = {
   siteMetadata: {
     title: 'Gatsby With Apollo',
-  },
-  plugins: [
-    {
-      resolve: 'gatsby-source-graphql',
-      options: {
-        typeName: 'RMAPI',
-        fieldName: 'rickAndMorty',
-        url: 'https://rickandmortyapi.com/graphql',
-      },
-    },
-  ],
+  }
 };
